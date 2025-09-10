1a63c5c229e822796766b262cc00e4680f146571
waldo1234567 <waldowalerian@gmail.com>
2025-09-10 23:53:45 +0800

    env setting

--- src/main/java/com/task_management/demo/DemoApplication.java
+++ src/main/java/com/task_management/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.task_management.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv =Dotenv.configure().directory("src/main/resources").ignoreIfMissing().load();
+		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
